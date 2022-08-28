ea5f23b1dfd131dd8fc61edf6f6d53b71c65cf6b
Marius BRAETS <mbraets@student.42.fr>
2022-08-28 14:37:12 +0200

    Fix problem with bullet

--- srcs/Game.cpp
+++ srcs/Game.cpp
@@ -215,17 +215,13 @@ void	Game::run( void )
 		if (tick % 10 == 0)
 		{
 			hurricanes.update(player);
-			Hurricane * current;
 			for (size_t i = 0; i < hurricanes.getData().size(); ++i)
 			{
-				current = &hurricanes.getData().at(i);
-				if (current == NULL)
-					continue;
 				if (tick % 50 == 0 && rand() % 2 == 0)
-					bullets.create(Source::SEnnemy, -1.0f, current->getPos().x, current->getPos().y);
+					bullets.create(Source::SEnnemy, -1.0f, hurricanes.getData().at(i).getPos().x, hurricanes.getData().at(i).getPos().y);
 			}
 		}
-		if (tick > 500 && tick % 50 == 0)
+		if (tick > 500 && tick % 1000 == 0)
 			hurricanes.create();
 
 		if (tick % 7 == 0)
