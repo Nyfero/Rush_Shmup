265236c800d67b934f2cdf8fa51d87a9190455c7
gsap <gaetan.sap@gmail.com>
2022-08-28 20:05:20 +0200

    merge

--- srcs/Game.cpp
+++ srcs/Game.cpp
@@ -242,7 +242,7 @@ void	Game::run( void )
 		
 		/*	UPDATE BULLET AND CHECK COLLISION	*/
 		
-		if (tick % 4 == 0)
+		if (tick % 2 == 0)
 			bullets.update();
 		
 		Bullet	*b = NULL;
@@ -425,48 +425,6 @@ void	Game::run( void )
 		usleep(9500);
 	}
 }
-
-// void	Game::checkCollision() {
-// 	for (size_t i = 0; i < bullets.getData().size(); ++i)
-// 	{
-// 		Bullet	*b = &bullets.getData().at(i);
-// 
-// 		//Check si balle player touche
-// 		if (bullets.getData().at(i).getSource() == Source::SPlayer)
-// 		{
-// 			for (size_t j = 0; j < hurricanes.getData().size(); ++j)
-// 			{
-// 				if (bullets.getData().at(i).getPos() == hurricanes.getData().at(j).getPos())
-// 				{
-// 					hurricanes.getData().at(j).clear();
-// 					hurricanes.getData().erase(j);
-// 					bullets.getData().at(i).clear();
-// 					bullets.getData().erase(i);
-// 					b = NULL;
-// 				}
-// 			}
-// 			for (size_t j = 0; b != NULL && j < tanas.getData().size(); ++j)
-// 			{
-// 				if (bullets.getData().at(i).getPos() == tanas.getData().at(j).getPos())
-// 				{
-// 					tanas.getData().at(j).clear();
-// 					tanas.getData().erase(j);
-// 					bullets.getData().at(i).clear();
-// 					bullets.getData().erase(i);
-// 				}
-// 			}
-// 		}
-// 		//Check si balle ennemi touche
-// 		else if (bullets.getData().at(i).getPos() == player.getPos())
-// 		{
-// 			player.update();
-// 			bullets.getData().at(i).clear();
-// 			bullets.getData().erase(i);
-// 			if (player.getLife() == 0)
-// 				_loop = false;
-// 		}
-// 	}
-// }
 
 Game::operator bool() const {
 	return (_status);
